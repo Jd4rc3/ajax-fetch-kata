3928d3ad56a217ea8755e226447fb5858b60db65
Jd4rc3 <jdarce91@misena.edu.co>
2022-08-03 10:25:13 -0500

    Saint service to perform calls to backend

---
+++ javascript/service/saint.service.mjs
@@ -0,0 +1,54 @@
+import { Saint } from "../model/saint.model.mjs";
+import { RequestHandler } from "./request.handler.mjs";
+
+/**
+ * Saints service class to get saints data from API
+ * @class
+ */
+export class SaintService {
+  /**
+   * URL of the API
+   */
+  #url;
+
+  constructor() {
+    this.#url = "http://localhost:3000/santos";
+  }
+
+  /**
+   * gets all saints from API
+   * @returns {Promise<Saint[]>}
+   */
+  async getSaints() {
+    const { data } = await fetch(this.#url);
+
+    return data.map(
+      ({ id, name, constellation }) => new Saint(id, name, constellation)
+    );
+  }
+
+  /**
+   * Creates a new saint in backend
+   * @param {Saint} saint
+   */
+  async createSaint(saint) {
+    await RequestHandler.sendData(this.#url, saint, "POST");
+  }
+
+  /**
+   * Update a saint in backend
+   * @param {number} id
+   * @param {Saint} data
+   */
+  async updateSaint(id, data) {
+    await RequestHandler.sendData(`${this.#url}/${id}`, data, "PUT");
+  }
+
+  /**
+   * Delete a saint in backend using the id
+   * @param {number} id
+   */
+  async deleteSaint(id) {
+    await RequestHandler.sendData(`${this.#url}/${id}`, {}, "DELETE");
+  }
+}
